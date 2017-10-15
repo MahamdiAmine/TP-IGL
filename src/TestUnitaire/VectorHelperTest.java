20c9f87f208d7e6c3055ab20b27771207f47a5e0
Mahamdi Amine <fm_mahamdi@esi.dz>
2017-10-15 21:58:31 +0100

    completer l'implimentation de toutes les fonctions de test

--- src/TestUnitaire/VectorHelperTest.java
+++ src/TestUnitaire/VectorHelperTest.java
@@ -22,4 +22,32 @@ public class VectorHelperTest {
         assertArrayEquals(new int[]{10,10,10,9,9,6,99,100010},vect3);
     }
 
+
+    @org.junit.Test
+    public void reverse() throws Exception {
+        int[] vect = new int[]{9, 5, 7, 2, 7, 2, 11, 5};
+        int[] vect2;
+        vect2=VectorHelper.reverse(vect);
+        assertArrayEquals(new int[]{5,11,2,7,2,7,5,9},vect2);
+    }
+
+    @org.junit.Test
+    public void minMax() throws Exception {
+        MinMax minmax;
+        int[] vect = new int[]{9, 5, 7, 2, 7, 2, 11, 5};
+        minmax= VectorHelper.minMax(vect);
+        int tab[]=new int [2];
+        tab[0]=minmax.getMin();
+        tab[1]=minmax.getMax();
+        assertArrayEquals(new int[]{2,11},tab);
+    }
+
+    @org.junit.Test
+    public void puissance() throws Exception {
+        int[] vect = new int[]{9, 5, 7, 2, 7, 2, 11, 5};
+        int p=2;
+        int []vect2=VectorHelper.puissance(vect,2);
+        assertArrayEquals(new int[]{81, 25, 49, 4, 49, 4, 121, 25},vect2);
+    }
+
 }
