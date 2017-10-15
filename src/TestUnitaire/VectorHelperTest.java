ab6028ae56a0899ca78fe0dfa56b409f44345807
Mahamdi Amine <fm_mahamdi@esi.dz>
2017-10-15 21:54:40 +0100

    Implimenter les testes unitaire pour les fonctions:tri et sommeVec

--- src/TestUnitaire/VectorHelperTest.java
+++ src/TestUnitaire/VectorHelperTest.java
@@ -5,6 +5,21 @@ import static org.junit.Assert.*;
  * @author Mahamdi Mohammed
  * @version 1.0.4
  */
+public class VectorHelperTest {
+    @org.junit.Test
+    public void tri() throws Exception {
+        int[] vect = new int[]{1, 5, 3, 7, 2, 4, 88, 100005};
+        VectorHelper.tri(vect);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 7, 88, 100005}, vect);
+    }
 
+    @org.junit.Test
+    public void sommeVec() throws Exception {
+        int[] vect1 = new int[]{1, 5, 3, 7, 2, 4, 88, 100005};
+        int[] vect2 = new int[]{9, 5, 7, 2, 7, 2, 11, 5};
+        int[] vect3;
+        vect3=VectorHelper.sommeVec(vect1,vect2);
+        assertArrayEquals(new int[]{10,10,10,9,9,6,99,100010},vect3);
+    }
 
 }
